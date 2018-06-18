236e5e7e6c21ab93d550d775bd4404728518e35e
libertylocked <unlock.liberty@gmail.com>
2018-06-18 19:21:12 -0400

    fix permission test comments

--- test/4-linnia-permissions-test.js
+++ test/4-linnia-permissions-test.js
@@ -4,6 +4,9 @@ import Linnia from '../src';
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 const testDataHash = '0x276bc9ec8730ad53e827c0467c00473a53337e2cb4b61ada24760a217fb1ef14';
+// TODO: replace URIs with strings like
+// /ipfs/QmUMqi1rr4Ad1eZ3ctsRUEmqK2U3CyZqpetUe51LB9GiAM
+// when contract artifacts are updated
 const testDataUri = '0x59742369c54039d5611d84452aa6c31b72da336b76ed4029b12c3dc5479836ba';
 const testMetaData = 'Blood_Pressure';
 const testSharedUri = '0xde1f76340a34698d41d362010bbc3c05c26f25d659904ef08ef7bd5eac0dbea4';
@@ -18,7 +21,6 @@ describe('Linnia-records', () => {
       gas: 4000000,
     });
     contracts = await linnia.getContractInstances();
-    // add a patient and a provider
     await contracts.users.register({ from: user1 });
     await contracts.users.register({ from: user2 });
     await contracts.users.register({ from: provider });
